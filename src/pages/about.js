9d59addc0c9b4b9a62a153a9ca8bead4e101d67e
Sara Alkhamri <s.alkhamri@gmail.com>
2020-10-30 13:36:05 -0700

    Created page componentes

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>Our Story</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
