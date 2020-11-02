c4babe4cf238d2043631e60c18aaefaee980a86e
Sara Alkhamri <s.alkhamri@gmail.com>
2020-11-02 12:02:34 -0800

    Added Footer to Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+
 import { Link } from "gatsby"
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -8,27 +9,37 @@ const ListLink = props => (
 
 export default function Layout({ children }) {
   return (
-    <div
-      style={{
-        margin: `3rem auto`,
-        maxWidth: 650,
-        padding: `0 1rem`,
-      }}
-    >
-      <header style={{ marginBottom: `1.5rem` }}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h1 style={{ display: `inline` }}>I. Create. Healthy</h1>
-        </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
-          {/* <ListLink to="/">Home</ListLink> */}
-          <ListLink to="/product">Product</ListLink>
-          <ListLink to="/blog">Blog</ListLink>
-          <ListLink to="/about/">About</ListLink>
-          <ListLink to="/contact/">Contact</ListLink>
-        </ul>
-      </header>
+    <>
+      <div
+        style={{
+          margin: `3rem auto`,
+          maxWidth: 650,
+          padding: `0 1rem`,
+        }}
+      >
+        <header style={{ marginTop: `1.5rem` }}>
+          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+            <h1 style={{ display: `inline` }}>I. Create. Healthy</h1>
+          </Link>
+          <ul style={{ listStyle: `none`, float: `right` }}>
+            {/* <ListLink to="/">Home</ListLink> */}
+            <ListLink to="/product">Product</ListLink>
+            <ListLink to="/blog">Blog</ListLink>
+            <ListLink to="/about/">About</ListLink>
+            <ListLink to="/contact/">Contact</ListLink>
+          </ul>
+        </header>
 
-      {children}
-    </div>
+        {children}
+      </div>
+      <div>
+        <footer style={{ position: `absolute`, bottom: 0 }}>
+          <small>
+            Made by <a href="https://saraalkhamri.com/"> Revive Labs </a>
+            copyright ©2020, All Rights Reserved
+          </small>
+        </footer>
+      </div>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 import main from "../assets/main.png"
-import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <div>
       <Layout />
-
       <img src={main} alt="Main" />
     </div>
   )
